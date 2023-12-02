b6407fc205449b07cfab3c3fb608b6063f6f981b
Sam Nordlinger <19315410+SNordlinger@users.noreply.github.com>
2023-12-01 22:09:43 -0800

    Day 2

---
+++ advent2023/day02.py
@@ -0,0 +1,78 @@
+import re
+import itertools
+
+GAME_RE = re.compile(r'Game \d+: ')
+
+
+def get_input():
+    with open('./data/day2.txt') as input_file:
+        return input_file.readlines()
+
+
+def parse_input(lines):
+    games = []
+    for line in lines:
+        stripped_line = re.sub(GAME_RE, '', line.strip())
+        game = [parse_subset(text) for text in stripped_line.split('; ')]
+        games.append(game)
+    return games
+
+
+def parse_subset(subset_text):
+    subset = {'red': 0, 'blue': 0, 'green': 0}
+    for entry in subset_text.split(', '):
+        (count, color) = parse_entry(entry)
+        subset[color] = count
+    return subset
+
+
+def parse_entry(entry_text):
+    parts = entry_text.split(' ')
+    return (int(parts[0]), parts[1])
+
+
+def sum_game(game):
+    entries = itertools.chain.from_iterable(game)
+    sums = {'red': 0, 'green': 0, 'blue': 0}
+    for (count, color) in entries:
+        sums[color] += count
+
+    return sums
+
+
+def check_game(game):
+    return all(sub['red'] <= 12 and sub['green'] <= 13 and sub['blue'] <= 14
+               for sub in game)
+
+
+def min_cube_power(game):
+    mins = {'red': 0, 'blue': 0, 'green': 0}
+    for sub in game:
+        mins['red'] = max(mins['red'], sub['red'])
+        mins['blue'] = max(mins['blue'], sub['blue'])
+        mins['green'] = max(mins['green'], sub['green'])
+    return mins['red'] * mins['blue'] * mins['green']
+
+
+def part1(games):
+    valid_sum = 0
+    for num, game in enumerate(games, start=1):
+        if check_game(game):
+            valid_sum += num
+
+    return valid_sum
+
+
+def part2(games):
+    return sum(min_cube_power(game) for game in games)
+
+
+def main():
+    lines = get_input()
+    games = parse_input(lines)
+    print(f'Part 1: {part1(games)}')
+    print(f'Part 2: {part2(games)}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_day02.py
@@ -0,0 +1,21 @@
+from advent2023.day02 import part1, parse_input, part2
+
+
+def test_part1():
+    sample_input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".splitlines()
+    games = parse_input(sample_input)
+    assert part1(games) == 8
+
+
+def test_part2():
+    sample_input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".splitlines()
+    games = parse_input(sample_input)
+    assert part2(games) == 2286
