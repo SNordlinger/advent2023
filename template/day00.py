ab5ad07b396d6f86fe53a3a6bc558dd13a81a8dc
Sam Nordlinger <19315410+SNordlinger@users.noreply.github.com>
2023-12-04 21:26:13 -0800

    Add templates

---
+++ template/day00.py
@@ -0,0 +1,26 @@
+def parse_input(lines):
+    pass
+
+
+def get_input():
+    with open('./data/day0.txt') as input_file:
+        return input_file.readlines()
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    lines = get_input()
+    data = parse_input(lines)
+    print(f'Part 1: {part1(data)}')
+    print(f'Part 2: {part2(data)}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ template/test_day00.py
@@ -0,0 +1,13 @@
+from advent2023.day00 import part1, parse_input, part2
+
+sample_input = """""".splitlines()
+
+
+def test_part1():
+    data = parse_input(sample_input)
+    # assert part1(data) ==
+
+
+def test_part2():
+    data = parse_input(sample_input)
+    # assert part2(data) == 
